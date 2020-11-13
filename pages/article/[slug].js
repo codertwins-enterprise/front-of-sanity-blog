a429a64f5e28928a2be23392f290c15d9e6f9d70
Görkem <gumutzkn@gmail.com>
2020-11-13 17:38:47 +0300

    Update [slug].js

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -9,10 +9,24 @@ const Post = (props) => {
     name = 'Missing name', 
     categories,
     body = []
-  } = props
+  } = props;
+
+  // const bodyText = props.body;
+
+
+  const serializers = {
+    types: {
+      block: props => (
+        <p className="blog__post--paragraph">
+          {props.children}
+        </p>
+      )
+    }
+  }
 
   return (
     <Layout title={title}>
+<<<<<<< Updated upstream
       <h1>{title}</h1>
       <span>By {name}</span>
       {categories && (
@@ -25,6 +39,36 @@ const Post = (props) => {
         blocks={body}
         {...client.config()}
       />
+=======
+      <section className="blog-container">
+        <div className="blog__posts">
+          <div className="blog__post">
+            <div class="blog__post--letter">{title[0]}</div>
+            <h1 className="heading-2">{title}</h1>
+            <span className="blog__post--date">
+              By {name} / 
+              {categories && (
+                <>
+                  In
+                  {categories.map(category => <span> {category}</span>)}
+                </>
+            )}
+            </span>                           
+            {/* {bodyText.map((user, index) => (
+              <p className="blog__post--paragraph" key={index}>
+                {user.children[0].text}
+              </p>
+            ))}    */}
+               
+            <BlockContent
+              blocks={body}
+              serializers={serializers}
+              {...client.config()}
+            />
+          </div>          
+        </div>
+      </section>      
+>>>>>>> Stashed changes
     </Layout>
   )
 }
